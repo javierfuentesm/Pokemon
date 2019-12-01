06acde3823a315642f46adf94cf085bbf1277223
rjfm <javier.fuentesm@hotmail.com>
2019-12-01 01:15:47 -0600

    Se agrega la opcion para dar bocadillos en el Safari

--- src/containers/Safari.jsx
+++ src/containers/Safari.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-
 import {
   Card,
   CardImg,
@@ -25,39 +24,58 @@ const randomId = () => {
   const max = Math.floor(807);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+const randomAction = () => {
+  const min = Math.ceil(1);
+  // Es el ultimo pokemon que esta en la PokeApi
+  const max = Math.floor(3);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 const Safari = ({ myPokemons, setMyPokemons }) => {
   const [pokemon, setPokemon] = useState(null);
+  const [bocadillo, setBocadillo] = useState(0);
+
   const fetchdata = async () => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId()}`);
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${randomId()}`,
+    );
     const data = await response.json();
     setPokemon(data);
   };
+
   useEffect(() => {
     fetchdata();
   }, [myPokemons]);
-  const capturePokemon = (pokemon) => {
-    alert(`Felicidades has capturado un ${pokemon.name}`);
-    setMyPokemons(pokemon);
-  };
 
+  const capturePokemon = (wildPokemon) => {
+    const action = randomAction();
+    if (action === 1) {
+      alert(`Felicidades has capturado un ${pokemon.name}`);
+      setMyPokemons(wildPokemon);
+      setBocadillo(0);
+    } else if (action === 2) {
+      alert('Parece que se ha salido de la pokebola intentalo de nuevo');
+    } else if (action === 3) {
+      alert('Parece que se ha huido');
+      setBocadillo(0);
+      fetchdata();
+    }
+  };
+  const giveBocadillo = () => {
+    alert('Le has dado un cadillo');
+    setBocadillo(bocadillo + 1);
+  };
   return (
     <Container>
       <hr />
       {pokemon ? (
         <Row>
           <Col sm={4}>
-            <Card
-              key={pokemon.id}
-              className="shadow p-3 mb-5 bg-white rounded"
-            >
+            <Card key={pokemon.id} className="shadow p-3 mb-5 bg-white rounded">
               <center>
                 <CardHeader>
                   <h1>
-                    {pokemon.name.replace(
-                      /^./,
-                      pokemon.name[0].toUpperCase(),
-                    )}
+                    {pokemon.name.replace(/^./, pokemon.name[0].toUpperCase())}
                   </h1>
                 </CardHeader>
 
@@ -69,30 +87,20 @@ const Safari = ({ myPokemons, setMyPokemons }) => {
                 />
                 <CardBody>
                   <CardTitle>
-                    <h3>
-Pokemón número
-                      {' '}
-                      {pokemon.id}
-                    </h3>
+                    <h3>{`Pokemón número ${pokemon.id}`}</h3>
                   </CardTitle>
                   <CardSubtitle>
                     <h5>Altura</h5>
                   </CardSubtitle>
-                  <CardText>
-                    {pokemon.height / 10}
-                    {' '}
-metros
-                  </CardText>
+                  <CardText>{`${pokemon.height / 10} metros`}</CardText>
                   <CardSubtitle>
                     <h5>Peso</h5>
                   </CardSubtitle>
-                  <CardText>
-                    {pokemon.weight / 10}
-                    {' '}
-kilos
-                  </CardText>
-
-
+                  <CardText>{`${pokemon.weight / 10} kilos`}</CardText>
+                  {/*  {bocadillo.length > 0 && ( */}
+                  {`Has dado ${bocadillo} bocadillos`}
+                  {/*  )} */}
+                  <CardText />
                 </CardBody>
               </center>
             </Card>
@@ -106,9 +114,8 @@ kilos
                     pokemon.name[0].toUpperCase(),
                   )} salvaje ha aparecido`}
                 </h1>
-
               </CardTitle>
-              <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+              <CardText>Parece que has tenido suerte el día de hoy.</CardText>
             </Card>
 
             <Card className="shadow p-5 mb-5 bg-white rounded">
@@ -116,15 +123,20 @@ kilos
                 <h3>¿Qué deseas hacer?</h3>
               </CardTitle>
               <CardText>
-                <Button color="success" onClick={() => capturePokemon(pokemon)}>Capturar</Button>
+                <Button color="success" onClick={() => capturePokemon(pokemon)}>
+                  Capturar
+                </Button>
                 &nbsp;
-                <Button color="danger" onClick={() => fetchdata()}>Dejar Ir</Button>
-
-
+                <Button color="danger" onClick={() => fetchdata()}>
+                  Huir
+                </Button>
+                &nbsp;
+                <Button color="warning" onClick={() => giveBocadillo()}>
+                  Dar bocadillo
+                </Button>
               </CardText>
             </Card>
           </Col>
-
         </Row>
       ) : (
         'Cargando  informacion'
