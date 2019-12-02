f5b93410248ad6821faebd4a51abde7cc9bfc099
rjfm <javier.fuentesm@hotmail.com>
2019-12-02 17:23:35 -0600

    Se mejora mi pc

--- src/containers/MiPc.jsx
+++ src/containers/MiPc.jsx
@@ -1,25 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import {
+  Container, Row, Col, ListGroup, ListGroupItem, Card, CardTitle, CardText,
+} from 'reactstrap';
 
 const MiPc = () => {
-  const myPokemons = useSelector(state => state.myPokemons);
+  const myPokemons = useSelector((state) => state.myPokemons);
   const dispatch = useDispatch();
 
   return (
     <Container>
+      <hr />
+      <Row>
+        <Card
+          className="shadow p-3 mb-5 bg-white rounded"
+          body
+        >
+          <CardTitle><h2>Mis Pokémon</h2></CardTitle>
+          <CardText>Aquá podrás visualizar los Pokémon que has capturado</CardText>
+        </Card>
+
+      </Row>
       <Row>
         <Col sm={12}>
           <ListGroup>
-            {myPokemons.map(pokemon => (
+            {myPokemons.map((pokemon) => (
               <ListGroupItem className="list-group-item d-flex justify-content-between align-items-center">
-                 {pokemon.name.replace(/^./, pokemon.name[0].toUpperCase())}
                 <img
                   src={pokemon.sprites.front_default}
                   alt="quixote"
-                  style={{ maxWidth: "10vh" }}
+                  style={{ maxWidth: '10vh' }}
                 />
+                <h3>
+                  {pokemon.name.replace(/^./, pokemon.name[0].toUpperCase())}
+                </h3>
+
               </ListGroupItem>
             ))}
           </ListGroup>
